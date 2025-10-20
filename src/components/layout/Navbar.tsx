6b2d122ba71369ec0b697214d5ab0500f11ee122
Venrite <146303521+Venrite@users.noreply.github.com>
2025-10-20 17:01:07 -0400

    fixing string to funciton data type error

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,13 @@
 import './navbar.css'
 import Button from '../ui/Button'
+function test(){
+    return null
+}
 export default function Navbar() {
     return (
         <div className="navbar">
             <h1>Zachary Gonzalez</h1>
-            <Button name={'Identity'} link={'LINK'}/> 
+            <Button name={'Identity'} link={test}/> 
         </div>
     )
 }
