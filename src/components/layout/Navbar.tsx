abcc758b2388e63c357bdff7a074d3251a2c554b
Venrite <146303521+Venrite@users.noreply.github.com>
2025-10-20 16:43:11 -0400

    fixing any data type issue

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="navbar">
             <h1>Zachary Gonzalez</h1>
-            <Button name={'Identity'} link={'LINK'}/>
+            <Button name={'Identity'} link={'LINK'}/> 
         </div>
     )
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,8 @@
-export default function Button({name,link}){
+type ButtonProp={
+    name:string,
+    link: ()=>null
+}
+export default function Button({name,link}:ButtonProp){
     return(
         <button onClick={link}>{name}</button>
     )
