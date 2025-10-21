5914eb98bad38b7249da414de3bffef0281d22df
Venrite <146303521+Venrite@users.noreply.github.com>
2025-10-21 16:18:51 -0400

    enhancing slab and preparing for Background image injection

--- src/app/pages/professional/components/Slab.tsx
+++ src/app/pages/professional/components/Slab.tsx
@@ -1,10 +1,19 @@
 import './componentStyles/slab.css'
-export default function Slab() {
+
+type SlabProp = {
+    desc?: string,
+    title: string,
+    link?: string
+}
+export default function Slab({ desc, title, link }: SlabProp) {
+    const backgroundStyle = link
+    ? { backgroundImage: `url(${link})`, backgroundSize: 'cover', backgroundPosition: 'center' }
+    : {}
     return (
-        <div className='slab'>
-            <div>desc</div>
-            <h1>title</h1>
-            <div>link</div>
+        <div className='slab' style={backgroundStyle}>
+            <div>{desc}</div>
+            <h1>{title}</h1>
+            <div>{link}</div>
         </div>
     )
 }
--- src/app/pages/professional/layouts/Skills.tsx
+++ src/app/pages/professional/layouts/Skills.tsx
@@ -5,10 +5,27 @@ import SplitScreen from '../../../../components/layout/SplitScreen'
 export default function Skills() {
     return (
         <div className="skills">
-            <Slab />
+            <Slab desc='desc' title='Front End Designer' link='link' />
             <SplitScreen
-                left={<Slab />}
-                right={<Slab />} />
+                left={
+                    <Slab desc='' title='title' link='src\assets\profile_test.png' />
+                }
+                right={<>
+                    <Slab desc='desc' title='title' link='link' />
+                    <Slab desc='desc' title='title' link='link' />
+                </>}
+            />
+            <SplitScreen
+                left={
+                    <>
+                    <Slab desc='desc' title='title' link='link' />
+                    <Slab desc='desc' title='title' link='link' />
+                </>
+                }
+                right={
+                    <Slab desc='desc' title='title' link='link' />
+                }
+            />
         </div>
     )
 }
