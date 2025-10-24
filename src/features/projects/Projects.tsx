bea09c9774ba4916a1a27867d86c064f81545f3b
Venrite <146303521+Venrite@users.noreply.github.com>
2025-10-24 10:10:36 -0400

    fixing unused import issue

--- src/features/projects/Projects.tsx
+++ src/features/projects/Projects.tsx
@@ -1,17 +1,9 @@
 import "./projects.css";
-import Slab from "../skills/components/slab/Slab";
 
 export default function Projects() {
   return (
     <div className="projects">
-      <Slab
-        desc="Chess made entirely using C99"
-        title="C Chess"
-        link="src\assets\profile_test.png"
-      />
-      <Slab desc="" title="title" link="src\assets\profile_test.png" />
-      <Slab desc="" title="title" link="src\assets\profile_test.png" />
-      <Slab desc="" title="title" link="src\assets\profile_test.png" />
+
     </div>
   );
 }
